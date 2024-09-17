373e08a21dda4b5d7cf1f9ec92c9c58e21013ef8
Pjiwm <63792136+Pjiwm@users.noreply.github.com>
2024-09-17 18:44:41 +0200

    download page and 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,17 @@
+<script>
+	import '../app.css';
+	import { page } from '$app/stores';
+</script>
+
+<div
+	class="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-gray-800 to-gray-900 text-white"
+>
+	<h1 class="text-9xl font-extrabold mb-4 text-red-500">404</h1>
+	<h1>{$page.status}: {$page.error.message}</h1>
+	<a
+		href="/"
+		class="mt-6 bg-red-500 text-white py-2 px-6 rounded-full text-lg font-semibold hover:bg-red-600 transition-all duration-300"
+	>
+		Go Back Home
+	</a>
+</div>
