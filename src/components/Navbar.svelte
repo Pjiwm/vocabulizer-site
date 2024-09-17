2c2fd97a641852b1a089cc2c0934ced5ea012efb
Pjiwm <63792136+Pjiwm@users.noreply.github.com>
2024-09-17 20:08:30 +0200

    navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<nav
+	class="fixed top-0 left-0 w-full bg-gray-900 border-b border-gray-700 py-4 px-6 flex items-center justify-between z-50"
+>
+	<!-- Logo and Title -->
+	<a href="/" class="flex items-center space-x-4">
+		<img src="/512x512.png" alt="Logo" class="w-10 h-10" />
+		<span class="text-white text-xl font-semibold">Vocabulizer</span>
+	</a>
+	<!-- Navigation Links -->
+	<div class="flex items-center space-x-6">
+		<a href="/" class="text-white text-lg flex items-center space-x-2 hover:underline">
+			<span>Home</span>
+		</a>
+		<a href="/buy" class="text-white text-lg hover:underline">Buy Now</a>
+	</div>
+</nav>
